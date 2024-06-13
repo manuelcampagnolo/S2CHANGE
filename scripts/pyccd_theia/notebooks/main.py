5ef3b0f9bc6e83ace4b78d85d008afe803a60f6d
Sara Caetano <150426225+scaetano1997@users.noreply.github.com>
2024-06-13 12:15:50 +0100

    Script main
    Guarda a série temporal em formato numpy com dimensões (número de imagens x número de bandas x número de pontos) e processa o CCD com respetiva validação.

---
+++ scripts/pyccd_theia/notebooks/main.py
@@ -0,0 +1,216 @@
+import os
+user_profile = os.environ['USERPROFILE']
+import logging
+
+directory_path = os.path.join(user_profile, 'Desktop', 'CCD_yml_win')
+os.chdir(directory_path)
+import geopandas as gpd
+import pandas as pd
+import os
+import sys
+from pathlib import Path
+# chamar python a partir da pasta 'CCD'
+module_path= Path(__name__ ).parent.absolute() / 'S2CHANGE' / 'scripts' / 'pyccd_theia' #  / 'CCD' / 'S2CHANGE' / 'scripts' / 
+base_path= Path(__name__ ).parent.absolute()  # dir do script; # dir referência (acima): 'DGT-S2CHANGE_2023'
+if module_path not in sys.path:
+    sys.path.append(str(module_path))
+import ccd
+from notebooks.avaliacao_exatidao_pyccd import filterDate, spatialJoin, preprocessCsvS2, valPol
+from notebooks.read_files import read_tif_files_theia, read_tif_files_gee, get_most_recent_file, readPoints
+from notebooks.processing import getTimeSeriesForPoints, runDetectionForPoint, processar_centros_pixeis
+from notebooks.utils import fromParamsReturnName
+from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import as_completed
+import warnings
+warnings.filterwarnings('ignore')
+import time
+import numpy as np
+import math
+#%%
+# Início da medição do tempo
+start_time = time.time()
+#%%
+public_documents = Path('C:/Users/Public/Documents/')
+
+samples = public_documents / 'inputs_pontos'
+pontos_input = 'pontos_300_buffers_1_metros.gpkg'
+caminho_arquivo = samples / pontos_input
+
+FOLDER_THEIA = public_documents / 'imagens_Theia' # Caminho dados THEIA
+FOLDER_GEE = public_documents / 'imagens_GEE' # Caminho dados GEE
+
+FOLDER_BDR = public_documents / 'BDR_300_artigo' / 'BDR_CCDC_TNE_Adjusted.shp' # Caminho para a base de dados de validação
+
+FOLDER_OUTPUTS = public_documents / 'output_BDR300'
+S2_tile = 'T29TNE'
+var = 'THEIA' # choose variable: THEIA or GEE
+
+if var == 'THEIA':
+    tiles = FOLDER_THEIA / S2_tile
+else:
+    tiles = FOLDER_GEE / S2_tile
+
+img_collection = tiles.parts[-2]
+
+# N=241941 # numero total de pontos presentes na BDR
+N = 100000
+
+random_state_value = 42
+
+bandas_desejadas = [1, 2, 3, 7, 9, 10]
+
+alpha = ccd.parameters.defaults['ALPHA'] # Looks for alpha in the parameters.py file
+ccd_params = ccd.parameters.defaults
+
+NODATA_VALUE= 65535
+
+# Parametros da validacao
+# datas do filtro das datas da análise (DGT 300)
+########### Não alterar ################
+dt_ini = '2018-09-12' # data inicial
+dt_end = '2021-09-30' # data final
+# Margem de tolerância entre a quebra do Modelo e do Analista
+theta = 60 # +/- theta dias de diferenca
+# bandar a filtrar com base na magnitude
+bandFilter = None #não implementado ainda - não mexer
+#%%
+def main(batch_size=None):
+    #abre geopackage com pontos
+    # print('A abrir o geopackage com pontos...')
+    raster_path = tiles / 'Theia_T29TNE_20170813-112433.tif'
+    
+    print('Processar centros dos pontos de cada geometria para corresponder aos centros dos pixels dos rasters...')
+    
+    start_time = time.time()
+
+    gdf_centros_pixeis = processar_centros_pixeis(FOLDER_BDR, raster_path)
+    
+    # Fim da execução do código
+    end_time = time.time()
+
+    # Calcula o tempo decorrido em segundos
+    execution_time_seconds = end_time - start_time
+
+    # Converte o tempo decorrido para minutos
+    execution_time_minutes = execution_time_seconds / 60
+
+    print("Processar centros dos pixels:", execution_time_minutes, "minutos")
+    
+    dados_geoespaciais_metros = readPoints(gdf_centros_pixeis, N, random_state_value)
+
+    #recolhe nome dos tifs e respetivas datas
+    print('A recolher nome e data dos tifs...')
+    
+    if var=='THEIA':
+        tif_names, tif_dates = read_tif_files_theia(S2_tile,tiles)
+    else:
+        tif_names, tif_dates = read_tif_files_gee(S2_tile,tiles)
+        
+    #add full path to tif names
+    tif_names = [os.path.join(tiles,i) for i in tif_names]
+    #convert dates to ordinal
+    tif_dates_ord = [d.toordinal() for d in tif_dates]
+    
+    print(f'Processando dados {var}... ({tiles})')
+    start_time = time.time()
+    #abre tifs com xarray e armazena informacao
+    print('A abrir tifs com xarray e carregar série temporal...')
+    
+    output_file = f"sel_values_{N}.npy"
+    dates = getTimeSeriesForPoints(tif_names, tif_dates_ord, bandas_desejadas, dados_geoespaciais_metros, output_file)
+
+    # Fim da execução do código
+    end_time = time.time()
+
+    # Calcula o tempo decorrido em segundos
+    execution_time_seconds = end_time - start_time
+
+    # Converte o tempo decorrido para minutos
+    execution_time_minutes = execution_time_seconds / 60
+
+    print(f"Ler dados {var} para um total de {N} pixels:", execution_time_minutes, "minutos")
+    
+    # Definir outras variáveis ​​necessárias para o processamento em lotes
+    num_pixels = N  # Número total de pixels
+    batch_size = 10000  # Tamanho do lote
+    num_batches = math.ceil(num_pixels / batch_size)  # Número de lotes necessários
+    
+    dfs = []
+    with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+        tqdm_bar = tqdm(total=num_batches)
+    
+        for batch_index, start_index in enumerate(range(0, num_pixels, batch_size)):
+            end_index = min(start_index + batch_size, num_pixels)
+    
+            # Carregar apenas o bloco atual de 5000 pontos
+            sel_values_block = np.load(output_file, mmap_mode='r')[:, :, start_index:end_index]
+            xs_slice = np.load(output_file + '_xs.npy', mmap_mode='r')[start_index:end_index]
+            ys_slice = np.load(output_file + '_ys.npy', mmap_mode='r')[start_index:end_index]
+            # xs_slice = xs[start_index:end_index]  # Fatiar xs
+            # ys_slice = ys[start_index:end_index]  # Fatiar ys
+    
+            # Cria argumentos para cada lote de 5000 pontos
+            arg_list = [(i, sel_values_block, dates, xs_slice, ys_slice, NODATA_VALUE, FOLDER_OUTPUTS, img_collection) for i in range(sel_values_block.shape[2])]
+    
+            for result_df in executor.map(runDetectionForPoint, arg_list):
+                dfs.append(result_df)
+                tqdm_bar.update(1)
+    
+        tqdm_bar.close()
+        
+    # Concatenar os resultados de todos os lotes em um único DataFrame
+    if dfs:
+        result_df = pd.concat(dfs, ignore_index=True)
+        filename = fromParamsReturnName(img_collection, ccd_params, (S2_tile,tiles), N, random_state_value)
+        result_df.to_csv(FOLDER_OUTPUTS / 'tabular' / '{}.csv'.format(filename), index=False)
+#%%
+def runValidation():
+    print('A correr validação dos resultados do ccd...')
+    filename = fromParamsReturnName(img_collection, ccd_params, (S2_tile,tiles), N, random_state_value)
+
+    csv_s2 = pd.read_csv(FOLDER_OUTPUTS / 'tabular' / '{}.csv'.format(filename))
+    #correr pre-processamento
+    csv_s2 = preprocessCsvS2(csv_s2)
+    csv_preprocessed_path = '{}_pre_proc.csv'.format(filename)
+    csv_s2.to_csv(csv_preprocessed_path)
+    
+
+    """## Filtrar datas
+    Limitar análise ao período considerado pelos analistas DGT
+    """
+    #correr filtro de datas
+    ccdcFiltro = filterDate(csv_preprocessed_path, dt_ini, dt_end, bandFilter)
+    """## Spatial join
+    Faz join dos pontos do csv com a informação de referencia da DGT (300 buffers). É associada aos pontos a informação da validação - data de alteração, tipo, classes, etc.
+    """
+    gdfVal = gpd.read_file(FOLDER_BDR)
+    gdfVal.to_crs(crs = 'EPSG:3763', inplace = True)
+    #executa o join
+    ccdcVal, ccdcVal_T = spatialJoin(FOLDER_BDR, ccdcFiltro)
+    """## Validação
+    Faz a validação da deteção - compara resultado do modelo (ccd) com dados de referência DGT
+    """ 
+    #faz a validação da deteção
+    DF_FINAL, DF_FINAL_T = valPol(ccdcVal_T, theta) #funcoes.valPol
+    """**Resultados da validação**"""
+    #delimita análise apenas para pontos referentes a transições entre Pinheiro Bravo e Eucalipto para Superfície sem vegetação, herbáceas e matos
+    #elimina também pontos da bordadura
+    df_aux = DF_FINAL_T.copy()
+    df_aux = df_aux.loc[(df_aux.altera=="Sem Alteracao")|((df_aux.altera=="Com Alteracao")&(df_aux.classeAnterior.isin(['Pinheiro bravo','Eucalipto']))&(df_aux.classeAtual.isin(['Superficie sem vegetacao escura','Superficie sem vegetacao clara','Vegetacao herbacea espontanea','Matos'])))]
+    df_aux = df_aux.loc[df_aux.bordadura==0]
+    #imprime f1-score, erro e omissão e erro de comissão
+    cm = df_aux.FP.sum()/(df_aux.FP.sum()+df_aux.VP.sum())
+    om = df_aux.FN.sum()/(df_aux.FN.sum()+df_aux.VP.sum())
+    f1 = 2*(1-om)*(1-cm)/(2-om-cm)
+    print(f'Alpha: {alpha}')
+    print(f'Random state value = {random_state_value}')
+    print('F1-score = {}%'.format(round(100*f1,2)))
+    print('Omission error = {}%'.format(round(100*om,2)))
+    print('Commission error = {}%'.format(round(100*cm,2)))
+
+    DF_FINAL_T.to_csv(FOLDER_OUTPUTS / 'tabular' / f'VAL_{filename}.csv', index=False)
+#%%
+if __name__ == '__main__':
+    main(batch_size=10000)
+    runValidation()
