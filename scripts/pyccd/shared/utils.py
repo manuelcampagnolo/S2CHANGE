6a803c4872a6bf602f6da9f67ad4bc185b1d4679
Daniel Moraes <45716860+danielm09@users.noreply.github.com>
2025-02-24 17:59:05 +0000

    Changes point-based run to polygon-based run

--- scripts/pyccd/shared/utils.py
+++ scripts/pyccd/shared/utils.py
@@ -1,13 +1,14 @@
 from notebooks.read_files import read_tif_files_theia, read_tif_files_gee
+import numpy as np
+from pathlib import Path
 
-def fromParamsReturnName(col_name, ccd_params, tifs_info, n_sample, random_state_value, min_year, max_date):
+def fromParamsReturnName(col_name, ccd_params, tifs_info, roi_name, min_year, max_date):
     """
     Returns file name based on execution parameters:
     col_name: image collection name (Theia, GEE).
     ccd_params: parameters found on ccd parameters.py file.
     tifs_info: path and tile name of tifs in image collection (in the for of (S2_tiles, tiles)).
-    n_sample: number of samples used when sampling input points.
-    random_state_value: seed used for sampling input points.
+    roi_name: name to identify the ROI (polygons) used in the run.
 
     NOTE: currently not implemented to return names of bands used for change detection and tmask.
     """
@@ -44,6 +45,18 @@ def fromParamsReturnName(col_name, ccd_params, tifs_info, n_sample, random_state
     start_date = min(dates).strftime("%Y%m%d")
     end_date = max(dates).strftime("%Y%m%d")
 
-    name = "{0}-NDVI_XX{1}YM{2}NOBS{3}LDA{4}ITER{5}_START{6}_END{7}_N{8}_RS{9}".format(col_name, chi, minYears, n_obs, lam, maxIter, start_date, end_date, n_sample, random_state_value)
+    name = "{0}-NDVI_XX{1}YM{2}NOBS{3}LDA{4}ITER{5}_START{6}_END{7}_ROI{8}".format(col_name, chi, minYears, n_obs, lam, maxIter, start_date, end_date, roi_name)
 
     return name
+
+def getNumberOfPixelsFromNpy(npy_path):
+    """
+    Returns the number of pixels based on the npy file.
+
+    Args:
+        npy_path : path to npy file.
+    """
+
+    aux = np.load(str(npy_path.with_suffix('')) + '_xs.npy') #opens xs because it is lighter
+
+    return aux.shape[0]
