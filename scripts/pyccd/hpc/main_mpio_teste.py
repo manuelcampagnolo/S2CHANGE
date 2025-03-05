02b11ab4a7a9d392b453eae80a041de1c5347ce8
saracaetano97 <156934905+saracaetano97@users.noreply.github.com>
2025-03-05 19:54:54 +0000

    Update main_mpio_teste.py

--- scripts/pyccd/hpc/main_mpio_teste.py
+++ scripts/pyccd/hpc/main_mpio_teste.py
@@ -23,7 +23,7 @@ if PASTA_DE_SCRIPTS not in sys.path:
     sys.path.append(str(PASTA_DE_SCRIPTS))
 import ccd
 from datetime import datetime
-from shared.processing import check_or_initialize_file, runDetectionForPoint#, create_geodataframe_from_parquet
+from shared.preprocessing import check_or_initialize_file, runDetectionForPoint#, create_geodataframe_from_parquet
 from shared.utils import fromParamsReturnName#, getNumberOfPixelsFromNpy
 from tqdm import tqdm
 import warnings
