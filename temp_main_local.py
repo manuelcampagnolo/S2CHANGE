d0110a7719960b80e0738d115876733cd2253d7c
Daniel Moraes <45716860+danielm09@users.noreply.github.com>
2025-03-09 17:02:45 +0000

    Add files via upload

---
+++ temp_main_local.py
@@ -0,0 +1,360 @@
+# PyCCD v01 - 24/02/2025 / Contrato N.ยบ 3044 DGT/ISA/CEXC/2152/2023
+import os
+import platform
+
+# Verifica o sistema operacional
+# Windows
+if platform.system() == "Windows":
+    user_profile = os.environ['USERPROFILE']
+    directory_path = os.path.join(user_profile, 'Desktop', 'S2CHANGE')
+else:  # Linux
+    user_home = os.path.expanduser("~")
+    directory_path = os.path.join(user_home, 'CCD_yml_win')
+os.chdir(directory_path)   
+import pandas as pd
+import rasterio
+import os
+import sys
+from pathlib import Path
+# Assumir onde esta a pasta dos scripts do PyCCD
+PASTA_DE_SCRIPTS = Path(__name__ ).parent.absolute() / 'scripts' / 'pyccd' 
+
+if PASTA_DE_SCRIPTS not in sys.path:
+    sys.path.append(str(PASTA_DE_SCRIPTS))
+import ccd
+from shared.avaliacao_exatidao_pyccd import runValidation
+from datetime import datetime
+from shared.processing import runDetectionForPoint, create_geodataframe_from_parquet
+from shared.preprocessing import check_or_initialize_file
+from shared.utils import fromParamsReturnName, getNumberOfPixelsFromNpy
+from shared.plot import plotFromCSV
+from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
+import warnings
+warnings.filterwarnings('ignore')
+import os
+import h5py
+# Working directory (DADOS):
+# |----FOLDER PUBLIC DOCUMENTS
+#    |---- SUBFOLDER BDR_300 (DGT)
+#         |---- file.shp
+#    |---- SUBFOLDER BDR_Navigator
+#         |---- file.gpkg
+#    |---- SUBFOLDER IMAGENS GEE
+#         |---- folder TILES
+#              |---- files.tif
+#    |---- SUBFOLDER IMAGENS THEIA
+#         |---- folder TILES
+#              |---- files.tif
+#    |---- SUBFOLDER output_BDR300
+#         |---- folder numpy
+#              |---- files.npy
+#         |---- folder plots
+#              |---- plots.png
+#         |---- folder tabular (csv e validaรงรฃo)
+#              |---- files.csv
+#    |---- SUBFOLDER output_NAV
+#         |---- folder numpy
+#              |---- files.npy
+#         |---- folder plots
+#              |---- plots.png
+#         |---- folder tabular (csv)
+#              |---- files.csv
+
+# Working directory (PyCCD):
+# |----FOLDER CCD_yml_win
+#    |---- SUBFOLDER S2CHANGE
+#       |---- SUBFOLDER scripts
+#         |---- SUBFOLDER pyccd_theia
+#           |---- SUBFOLDER ccd
+#              |---- SUBFOLDER models
+#                   |---- __init__.py
+#                   |---- lasso.py
+#                   |---- robust_fit.py
+#                   |---- tmask.py
+#              |---- __init__.py
+#              |---- app.py
+#              |---- change.py
+#              |---- math_utils.py
+#              |---- parameters.py
+#              |---- procedures.py
+#              |---- qa.py
+#              |---- version.py
+#           |---- SUBFOLDER notebooks 
+#              |---- addNewImageToFile.py
+#              |---- avaliacao_exatidao_pyccd.py
+#              |---- main.py (** ficheiro principal **)
+#              |---- plot.py
+#              |---- processing.py
+#              |---- read_files.py
+#              |---- utils.py
+#%%
+# ---------------------------------
+#             INPUTS
+# ---------------------------------
+var = 'THEIA' # choose variable: THEIA or GEE
+S2_tile = 'T29TNE' # escolher o tile S2
+
+# Caminho onde estรฃo os inputs todos
+PASTA_DE_INPUTS = Path(r'C:/Users/Public/Documents/imagens_THEIA/')#Path('D:/')
+# -> Shapefile ou Geopackage que contem a regiao de interesse
+# REGIAO_INTERESSE =  PASTA_DE_INPUTS / 'BDR_Navigator' / 'nvg_2018_ccd.gpkg'
+REGIAO_INTERESSE = Path('C:/Users/scaetano/Downloads/CCDC_Mask_dissolve.gpkg')
+# Nome para identificar a BDR
+BDR = "NAV"
+# -> IMAGENS SENTINEL:
+IMAGENS_S2 = PASTA_DE_INPUTS #/ 's2_images'
+
+tiles = IMAGENS_S2 / S2_tile # tiff files
+batch_size = 1000 # Número de pixels para cada lote -- to be done sequentially on each core (either hpc or local)
+
+# ---------------------------------
+#   PARAMETROS PRE PROCESSAMENTO  / PYCCD
+# ---------------------------------
+min_year =  2017 # ano inicial da corrida do CCD
+max_date = datetime(2024, 12, 31) # data atรฉ onde se corre o ccd
+#bandas_desejadas = [1, 2, 3, 7, 10] # bandas usadas para o pré-processamento
+input_bands = ['B3', 'B4', 'B8', 'B12']
+bands_dict = {1:'B3', 2:'B4', 3:'B8', 4:'B12'} # a banda NDVI só é adicionada mais à frente na funcao processPointData
+bandas_desejadas = list(bands_dict.keys())
+
+NODATA_VALUE = 65535
+MAX_VALUE_NDVI = 10000
+
+EXECUTAR_PLOT = False # (false para nรฃo fazer; true para fazer)
+ROW_INDEX = 8 # plot para uma linha do CSV (escolher a linha no row_index)
+
+img_collection = tiles.parts[-2]
+
+CRS_THEIA = 32629
+CRS_WGS84 = 4326
+# ---------------------------------
+#            OUTPUTS
+# ---------------------------------
+PASTA_DE_OUTPUTS = Path('C:/Users/Public/Documents/outputs_RI')
+FOLDER_NPY = PASTA_DE_OUTPUTS / 'numpy' / S2_tile
+FOLDER_PLOTS = PASTA_DE_OUTPUTS / 'plots' / S2_tile
+FOLDER_PARQUET = PASTA_DE_OUTPUTS / 'tabular' / S2_tile
+#FOLDER_SHP = PASTA_DE_OUTPUTS / 'shapefiles' / S2_tile
+
+# Funรงรฃo para criar diretรณrios se nรฃo existirem
+def create_directory_if_not_exists(path):
+    if not path.exists():
+        path.mkdir(parents=True, exist_ok=True)
+        print(f"Diretorio criado: {path}")
+    else:
+        print(f"Diretorio ja existe: {path}")
+
+# ---------------------------------
+#      PARAMETROS PROCESSAMENTO / only needs to be done once for dreating npy/hdf5 files
+# ---------------------------------
+
+
+# ---------------------------------
+#          PARAMETROS CCD / PYCCD
+# ---------------------------------
+alpha = ccd.parameters.defaults['ALPHA'] # Looks for alpha in the parameters.py file
+ccd_params = ccd.parameters.defaults
+######### NOME BASE DOS FICHEIROS A SEREM GERADOS #########
+
+# BDR is an arbitrary name for ROI
+filename = fromParamsReturnName(img_collection, ccd_params, (S2_tile, tiles), BDR, min_year, max_date)
+
+############ OUTPUTS ###################### need to be consisten and -- 'h5' should be an option
+output_file = FOLDER_NPY / "{}.h5".format(filename) # ficheiro numpy (matriz) dos dados (nr de imagens x nr de bandas x nr total de pontos)
+
+"""
+# ---------------------------------
+#      PARAMETROS DA VALIDAรรO
+# ---------------------------------
+# datas do filtro das datas da anรกlise (DGT 300)
+########### Nรฃo alterar ################
+dt_ini = '2018-09-12' # data inicial
+dt_end = '2021-09-30' # data final
+# Margem de tolerรขncia entre a quebra do Modelo e do Analista
+theta = 60 # +/- theta dias de diferenca
+# banda a filtrar com base na magnitude
+bandFilter = None #nรฃo implementado ainda - nรฃo mexer
+#%%
+"""
+
+def main(batch_size):
+    # Criar os diretรณrios
+    create_directory_if_not_exists(FOLDER_NPY)
+    create_directory_if_not_exists(FOLDER_PLOTS)
+    create_directory_if_not_exists(FOLDER_PARQUET)
+    #create_directory_if_not_exists(FOLDER_SHP)
+
+    # This should be moved within check_or_initialize_file (which is shared) 
+    # Listar todos os ficheiros na pasta e ordenar pelo tamanho (maior primeiro)
+    raster_files = sorted(tiles.glob('*.*'), key=lambda f: f.stat().st_size, reverse=True)
+
+    # Se a lista de arquivos não estiver vazia, escolher o maior arquivo
+    raster_path = None
+    if raster_files:
+        largest_file = raster_files[0]
+        try:
+            with rasterio.open(largest_file) as src:
+                if src.read(1).size > 0:  # Verificar se a imagem tem dados validos
+                    raster_path = largest_file
+        except:
+            raster_path = None  # Se houver erro ao abrir, nada é selecionado
+
+    # Imprimir o tiff selecionado
+    if raster_path:
+        print("Imagem selecionada:", raster_path)
+    else:
+        print("Nenhuma imagem valida foi encontrada.")
+    # Verificar a existência do arquivo .npy e inicializar ou carregar os dados
+    # Create npy/h5 file if "output_file doesn't exist
+    # obs: calculation of raster_path should be included in check_or_initialize_file
+    tif_dates_ord, N = check_or_initialize_file(output_file, tiles, var, S2_tile, min_year, max_date, REGIAO_INTERESSE, 
+                                                bandas_desejadas, PASTA_DE_OUTPUTS, img_collection, NODATA_VALUE, raster_path)
+    #N = 24000
+    #batch_size = 1000
+    indices = list(range(0, N, batch_size))
+    batches = [
+        (start, min(start + batch_size, N)) for start in indices
+    ]
+    size = os.cpu_count()
+    batches_per_process = [batches[i::size] for i in range(size)]
+    #print('num batches = {}'.format(len(batches)))
+
+    #print(batches_per_process)
+    args = [(batch, output_file, tif_dates_ord) for batch in batches_per_process]
+    
+    import time
+    tstart = time.time()
+    dfs = []
+    with ProcessPoolExecutor(max_workers=os.cpu_count()) as executor:
+        tqdm_bar = tqdm(total=len(batches))
+        
+        # Processar os batches paralelamente
+        # function process_batch defined below
+        for batch_results in executor.map(process_batch, args):
+            dfs.extend(batch_results)  # Adiciona os resultados processados; dataframe or list of dataframes
+            tqdm_bar.update(1)
+        
+        tqdm_bar.close()
+    print('parallel execution took {}s'.format(round(time.time()-tstart,2)))
+    exit()
+    # Concatenar os resultados de todos os lotes num único DataFrame  // dataframe could be problematic 
+    # try to write this as a fucntion with imnput: dfs; output: parquet file path
+    if dfs:
+        result_df = pd.concat(dfs, ignore_index=True)
+        for col in result_df.columns:
+            if result_df[col].apply(lambda x: isinstance(x, list)).any():
+                result_df = result_df.explode(col)
+        print(f"Saving the parquet file with {len(result_df)} records.")
+        result_df.to_parquet(FOLDER_PARQUET / '{}.parquet'.format(filename), index=False)
+    
+    #if EXECUTAR_PLOT:
+    #    plotFromCSV(FOLDER_PARQUET / '{}.parquet'.format(filename), ROW_INDEX, save_dir=FOLDER_PLOTS / '{}_RowIndex{}.png'.format(filename, ROW_INDEX))
+
+import time
+# Função auxiliar para processar um batch
+def process_batch(args):
+    """
+    Processes a batch of points for change detection.
+
+    This function takes a batch of selected values and processes each point in the batch 
+    by calling the `runDetectionForPoint` function. It creates a list of arguments for each 
+    point in the batch and returns the results for all points.
+
+    Args:
+        - args (tuple): A tuple containing the following elements:
+            - i (int): Index of the point being processed within the batch. Used to iterate through each point in the batch.
+            - sel_values_block (ndarray): 3D array of selected values for the batch (number of images x number of bands x batch_size).
+            - xs_slice (ndarray): Array of x coordinates for the batch.
+            - ys_slice (ndarray): Array of y coordinates for the batch.
+            - tif_dates_ord (ndarray): Array of ordered dates corresponding to the images.
+            - NODATA_VALUE (float): Value representing missing data in the dataset.
+            - MAX_VALUE_NDVI (float): Maximum possible NDVI value used for scaling.
+            - PASTA_DE_OUTPUTS (str): Directory to save the output results.
+            - CRS_THEIA (str): Coordinate Reference System (CRS) for the Theia dataset.
+            - CRS_WGS84 (str): Coordinate Reference System (CRS) for the WGS84 standard.
+            - img_collection (ndarray): Collection of Sentinel-2 images.
+
+    Returns:
+        - list: A list of results from `runDetectionForPoint` for each point in the batch.
+    """
+    batches, sel_values_path, tif_dates_ord = args
+    results = []
+    with h5py.File(sel_values_path, 'r') as h5_file:
+        
+        count = 0
+
+        for batch in batches:
+            tstart = time.time()
+            start, end = batch
+            sel_values = h5_file['values'][:,:,start:end]
+            xs = h5_file['xs'][start:end]
+            ys = h5_file['ys'][start:end]
+            # sel_values = np.ascontiguousarray(sel_values)
+            # xs = np.ascontiguousarray(xs)
+            # ys = np.ascontiguousarray(ys)
+            arg_list = [(i, sel_values, tif_dates_ord, xs, ys,NODATA_VALUE, MAX_VALUE_NDVI, PASTA_DE_OUTPUTS, CRS_THEIA, CRS_WGS84, img_collection)
+                        for i in range(end-start)]
+            results.append([runDetectionForPoint(arg) for arg in arg_list])
+            count = count+1
+            print('count = {}/{}. time per batch = {}s'.format(count, len(batches), round(time.time()-tstart,2)))
+
+    return results
+
+import numpy as np
+def process_batch_npy(args):
+    """
+    Processes a batch of points for change detection.
+
+    This function takes a batch of selected values and processes each point in the batch 
+    by calling the `runDetectionForPoint` function. It creates a list of arguments for each 
+    point in the batch and returns the results for all points.
+
+    Args:
+        - args (tuple): A tuple containing the following elements:
+            - i (int): Index of the point being processed within the batch. Used to iterate through each point in the batch.
+            - sel_values_block (ndarray): 3D array of selected values for the batch (number of images x number of bands x batch_size).
+            - xs_slice (ndarray): Array of x coordinates for the batch.
+            - ys_slice (ndarray): Array of y coordinates for the batch.
+            - tif_dates_ord (ndarray): Array of ordered dates corresponding to the images.
+            - NODATA_VALUE (float): Value representing missing data in the dataset.
+            - MAX_VALUE_NDVI (float): Maximum possible NDVI value used for scaling.
+            - PASTA_DE_OUTPUTS (str): Directory to save the output results.
+            - CRS_THEIA (str): Coordinate Reference System (CRS) for the Theia dataset.
+            - CRS_WGS84 (str): Coordinate Reference System (CRS) for the WGS84 standard.
+            - img_collection (ndarray): Collection of Sentinel-2 images.
+
+    Returns:
+        - list: A list of results from `runDetectionForPoint` for each point in the batch.
+    """
+    batches, sel_values_path, tif_dates_ord = args
+    results = []
+    count = 0
+    for batch in batches:
+        tstart = time.time()
+        start, end = batch
+        sel_values = np.load(sel_values_path, mmap_mode='r')[:, :, start:end]
+        xs = np.load(str(sel_values_path.with_suffix('')) + '_xs.npy', mmap_mode='r')[start:end]
+        ys = np.load(str(sel_values_path.with_suffix('')) + '_ys.npy', mmap_mode='r')[start:end]
+        arg_list = [(i, sel_values, tif_dates_ord, xs, ys,NODATA_VALUE, MAX_VALUE_NDVI, PASTA_DE_OUTPUTS, CRS_THEIA, CRS_WGS84, img_collection)
+                        for i in range(end-start)]
+        results.append([runDetectionForPoint(arg) for arg in arg_list])
+        count = count+1
+        print('count = {}/{}. time per batch = {}s'.format(count, len(batches), round(time.time()-tstart,2)))
+
+    return results
+
+def process_in_series(args):
+
+    sel_values, tif_dates_ord, xs, ys, N = args
+
+    arg_list = [(i, sel_values, tif_dates_ord, xs, ys,NODATA_VALUE, MAX_VALUE_NDVI, PASTA_DE_OUTPUTS, CRS_THEIA, CRS_WGS84, img_collection)
+                        for i in range(N)]
+
+    return [runDetectionForPoint(arg) for arg in arg_list]
+#%%
+
+#%%
+# Executar o código
+if __name__ == '__main__':
+    main(batch_size)
