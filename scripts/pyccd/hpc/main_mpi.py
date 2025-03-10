795162d850b5692cc0f8a3770cf7b0450bb59bb1
domwelsh <domwelsh@gmail.com>
2025-03-10 14:50:14 +0000

    Updated main_mpi.py based on main_mpi_cleanup.py from main_cleanup branch

--- scripts/pyccd/hpc/main_mpi.py
+++ scripts/pyccd/hpc/main_mpi.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
+#%% Imports and Path Setup
+# Standard library imports
 import os
+import sys
 import platform
-# Verifica o sistema operacional
-# Windows
+from pathlib import Path
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import Manager
+import warnings
+
+# Set up path for custom modules
 if platform.system() == "Windows":
     user_profile = os.environ['USERPROFILE']
     directory_path = os.path.join(user_profile, 'Desktop', 'S2CHANGE')
@@ -10,115 +17,30 @@ else:  # Linux
     user_home = os.path.expanduser("~")
     directory_path = os.path.join(user_home, 'S2CHANGE')
 os.chdir(directory_path)
-import pandas as pd
-import rasterio
-import sys
-from pathlib import Path
-# Assumir onde esta a pasta dos scripts do PyCCD
-PASTA_DE_SCRIPTS = Path(__name__ ).parent.absolute() / 'scripts' / 'pyccd' 
+
+# PyCCD script folder path
+PASTA_DE_SCRIPTS = Path(__name__).parent.absolute() / 'scripts' / 'pyccd'
 if PASTA_DE_SCRIPTS not in sys.path:
     sys.path.append(str(PASTA_DE_SCRIPTS))
-import ccd
-from datetime import datetime
-from shared.preprocessing import check_or_initialize_file
-from shared.processing import runDetectionForPoint, explode_columns#, create_geodataframe_from_parquet
-from shared.utils import fromParamsReturnName#, getNumberOfPixelsFromNpy
-from tqdm import tqdm
-import warnings
-warnings.filterwarnings('ignore')
-from mpi4py import MPI
-import platform
-from multiprocessing import Manager
-from pathlib import Path
+
+# Third-party libraries
+import pandas as pd
 import h5py
+from tqdm import tqdm
+from mpi4py import MPI
+
+# PyCCD module imports
+from shared.processing import runDetectionForPoint, explode_columns
+from shared.preprocessing import check_or_initialize_file
+from config.config import input_config, preprocessing_config, outputs_config, ccd_config
+
+# Environment variables
 cpus_slurm = int(os.getenv('SLURM_NTASKS', os.cpu_count()))
+
+# Suppress warnings
+warnings.filterwarnings('ignore')
 #%%
-# ---------------------------------
-#             INPUTS
-# ---------------------------------
-var = 'GEE' # choose variable: THEIA or GEE
-BDR = 'NAV' # choose variable: DGT or NAV
-S2_tile = 'T29SPB' # escolher o tile S2
-# Caminho onde estao os dados todos
-public_documents = Path('/users1/cpca070342024/scaetano')
-# Caminhos para a base de dados de validacao
-BDR_FILE = Path('/users1/cpca070342024/scaetano/CCDC_Mask_dissolve.gpkg')
-# -> IMAGENS SENTINEL:
-FOLDER_THEIA = public_documents / 'imagens_THEIA' # Caminho dados THEIA
-FOLDER_GEE = public_documents / 's2_images' # Caminho dados GEE
 
-if var == 'THEIA':
-    tiles = FOLDER_THEIA / S2_tile
-else:
-    tiles = FOLDER_GEE / S2_tile
-#%% 
-# ---------------------------------
-#   PARAMETROS PRE PROCESSAMENTO
-# ---------------------------------
-min_year =  2017 # ano inicial da corrida do CCD
-max_date = datetime(2023, 9, 29) # data atÃ© onde se corre o ccd
-
-input_bands=['B3', 'B4', 'B8', 'B12']
-bands_dict={1: 'NDVI', 2: 'B3', 3:'B4', 4: 'B8', 5:'B12'}
-bandas_desejadas = bands_dict.keys() # to check
-
-NODATA_VALUE = 65535
-MAX_VALUE_NDVI = 10000
-
-EXECUTAR_PLOT = False # (false para nÃ£o fazer; true para fazer)
-ROW_INDEX = 8 # plot para uma linha do CSV (escolher a linha no row_index)
-
-BATCH_SIZE = 100  # Ajustar o tamanho do lote para processamento em paralelo
-
-img_collection = tiles.parts[-2]
-
-CRS_THEIA = 32629
-CRS_WGS84 = 4326
-# ---------------------------------
-#            OUTPUTS
-# ---------------------------------
-FOLDER_OUTPUTS = Path('/users1/cpca070342024/scaetano/outputs_RI')
-
-FOLDER_NPY = FOLDER_OUTPUTS / 'numpy' / S2_tile
-FOLDER_PLOTS = FOLDER_OUTPUTS / 'plots' / S2_tile
-FOLDER_PARQUET = FOLDER_OUTPUTS / 'tabular' / S2_tile
-FOLDER_SHP = FOLDER_OUTPUTS / 'shapefiles' / S2_tile
-
-# FunÃ§Ã£o para criar diretÃ³rios se nÃ£o existirem
-def create_directory_if_not_exists(path):
-    if not path.exists():
-        path.mkdir(parents=True, exist_ok=True)
-
-# Criar os diretÃ³rios
-create_directory_if_not_exists(FOLDER_NPY)
-create_directory_if_not_exists(FOLDER_PLOTS)
-create_directory_if_not_exists(FOLDER_PARQUET)
-create_directory_if_not_exists(FOLDER_SHP)
-
-# ---------------------------------
-#      PARAMETROS PROCESSAMENTO
-# ---------------------------------
-raster_files = sorted(tiles.glob('*.*'), key=lambda f: f.stat().st_size, reverse=True)
-
-raster_path = None
-if raster_files:
-    largest_file = raster_files[0] 
-    try:
-        with rasterio.open(largest_file) as src:
-            if src.read(1).size > 0:  # Verificar se a imagem tem dados vÃ¡lidos
-                raster_path = largest_file
-    except:
-        raster_path = None  # Se houver erro ao abrir, nada Ã© selecionado
-# ---------------------------------
-#          PARAMETROS CCD
-# ---------------------------------
-alpha = ccd.parameters.defaults['ALPHA'] # Looks for alpha in the parameters.py file
-ccd_params = ccd.parameters.defaults
-######### NOME BASE DOS FICHEIROS A SEREM GERADOS #########
-filename = fromParamsReturnName(img_collection, ccd_params, (S2_tile, tiles), BDR, min_year, max_date)
-############ OUTPUTS ######################
-output_file = FOLDER_NPY / "{}.h5".format(filename) # ficheiro numpy (matriz) dos dados (nr de imagens x nr de bandas x nr total de pontos)
-#%%
 def main(batch_size=None):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
@@ -127,9 +49,15 @@ def main(batch_size=None):
     if rank == 0:
         # root rank
         tif_dates_ord, N = check_or_initialize_file(
-            output_file, tiles, var, S2_tile, min_year, max_date, BDR_FILE,
-            bandas_desejadas, FOLDER_OUTPUTS, img_collection, NODATA_VALUE, raster_path
-        )
+            outputs_config['output_file'], 
+            input_config['tiles'], 
+            input_config['data_source_folder'], 
+            input_config['s2_tile_folder'], 
+            preprocessing_config['min_year'], 
+            preprocessing_config['max_date'], 
+            input_config['roi'], 
+            preprocessing_config['bandas_desejadas']
+            )
         
         N=1000000
         
@@ -165,7 +93,7 @@ def main(batch_size=None):
                     pbar.update(1)
         
             for batch in my_batches:
-                result = process_batch(batch, output_file, tif_dates_ord)
+                result = process_batch(batch, outputs_config['output_file'], tif_dates_ord)
                 local_results.extend(result)
                 update_progress(lock)
 
@@ -173,7 +101,7 @@ def main(batch_size=None):
     if local_results:
         result_df = pd.concat(local_results, ignore_index=True)
         result_df = explode_columns(result_df)
-        result_df.to_parquet(FOLDER_PARQUET / f'{filename}_rank_{rank}.parquet', index=False)
+        result_df.to_parquet(outputs_config['folders']['tabular'] / f'{ccd_config['filename']}_rank_{rank}.parquet', index=False)
 
     # Sync all ranks before continuing
     comm.Barrier()
@@ -202,11 +130,21 @@ def process_batch(batch, sel_values_path, tif_dates_ord):
     ys_slice = h5_file['ys'][start:end]
 
     arg_list = [
-        (i, sel_values_block, tif_dates_ord, xs_slice, ys_slice, NODATA_VALUE, MAX_VALUE_NDVI, FOLDER_OUTPUTS, CRS_THEIA, CRS_WGS84, img_collection)
+        (i, 
+        sel_values_block, 
+        tif_dates_ord, 
+        xs_slice, 
+        ys_slice, 
+        preprocessing_config['nodata_value'], 
+        preprocessing_config['max_value_ndvi'], 
+        outputs_config['output_path'], 
+        preprocessing_config['crs_theia'], 
+        preprocessing_config['wgs84'], 
+        preprocessing_config['img_collection'])
         for i in range(sel_values_block.shape[2])
     ]
 
     return [runDetectionForPoint(arg) for arg in arg_list]
 
 if __name__ == '__main__':
-    main(BATCH_SIZE)
+    main(preprocessing_config['batch_size'])
