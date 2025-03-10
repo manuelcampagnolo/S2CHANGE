bd203a7b3653e630f8bf4b848de39a1070035bbd
saracaetano97 <156934905+saracaetano97@users.noreply.github.com>
2025-03-10 11:26:15 +0000

    Update main_mpi.py

--- scripts/pyccd/hpc/main_mpi.py
+++ scripts/pyccd/hpc/main_mpi.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-# TESTE HPC MPI COM HDF5 ---> INCD
 import os
 import platform
-
 # Verifica o sistema operacional
 # Windows
 if platform.system() == "Windows":
@@ -18,20 +16,19 @@ import sys
 from pathlib import Path
 # Assumir onde esta a pasta dos scripts do PyCCD
 PASTA_DE_SCRIPTS = Path(__name__ ).parent.absolute() / 'scripts' / 'pyccd' 
-from concurrent.futures import ProcessPoolExecutor
 if PASTA_DE_SCRIPTS not in sys.path:
     sys.path.append(str(PASTA_DE_SCRIPTS))
 import ccd
 from datetime import datetime
 from shared.preprocessing import check_or_initialize_file
 from shared.processing import runDetectionForPoint#, create_geodataframe_from_parquet
-from shared.utils import fromParamsReturnName#, getNumberOfPixelsFromNpy
+from shared.utils import fromParamsReturnName, explode_columns #, getNumberOfPixelsFromNpy
 from tqdm import tqdm
 import warnings
 warnings.filterwarnings('ignore')
 from mpi4py import MPI
 import platform
-from multiprocessing import Manager, Lock
+from multiprocessing import Manager
 from pathlib import Path
 import h5py
 cpus_slurm = int(os.getenv('SLURM_NTASKS', os.cpu_count()))
@@ -39,9 +36,9 @@ cpus_slurm = int(os.getenv('SLURM_NTASKS', os.cpu_count()))
 # ---------------------------------
 #             INPUTS
 # ---------------------------------
-var = 'THEIA' # choose variable: THEIA or GEE
+var = 'GEE' # choose variable: THEIA or GEE
 BDR = 'NAV' # choose variable: DGT or NAV
-S2_tile = 'T29TNE' # escolher o tile S2
+S2_tile = 'T29SPB' # escolher o tile S2
 # Caminho onde estao os dados todos
 public_documents = Path('/users1/cpca070342024/scaetano')
 # Caminhos para a base de dados de validacao
@@ -59,7 +56,7 @@ else:
 #   PARAMETROS PRE PROCESSAMENTO
 # ---------------------------------
 min_year =  2017 # ano inicial da corrida do CCD
-max_date = datetime(2024, 12, 31) # data atÃ© onde se corre o ccd
+max_date = datetime(2023, 9, 29) # data atÃ© onde se corre o ccd
 
 input_bands=['B3', 'B4', 'B8', 'B12']
 bands_dict={1: 'NDVI', 2: 'B3', 3:'B4', 4: 'B8', 5:'B12'}
@@ -71,7 +68,7 @@ MAX_VALUE_NDVI = 10000
 EXECUTAR_PLOT = False # (false para nÃ£o fazer; true para fazer)
 ROW_INDEX = 8 # plot para uma linha do CSV (escolher a linha no row_index)
 
-BATCH_SIZE = 50  # Ajustar o tamanho do lote para processamento em paralelo
+BATCH_SIZE = 100  # Ajustar o tamanho do lote para processamento em paralelo
 
 img_collection = tiles.parts[-2]
 
@@ -121,8 +118,7 @@ ccd_params = ccd.parameters.defaults
 filename = fromParamsReturnName(img_collection, ccd_params, (S2_tile, tiles), BDR, min_year, max_date)
 ############ OUTPUTS ######################
 output_file = FOLDER_NPY / "{}.h5".format(filename) # ficheiro numpy (matriz) dos dados (nr de imagens x nr de bandas x nr total de pontos)
-
-# Função principal
+#%%
 def main(batch_size=None):
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
@@ -134,7 +130,9 @@ def main(batch_size=None):
             output_file, tiles, var, S2_tile, min_year, max_date, BDR_FILE,
             bandas_desejadas, FOLDER_OUTPUTS, img_collection, NODATA_VALUE, raster_path
         )
-
+        
+        N=1000000
+        
         # Dividir os ÃƒÂ­ndices de dados
         indices = list(range(0, N, batch_size))
         batches = [
@@ -148,75 +146,67 @@ def main(batch_size=None):
         tif_dates_ord = None
         batches_per_rank = None
 
-    # Compartilhar dados entre processos
+    # Share data between processes
     tif_dates_ord = comm.bcast(tif_dates_ord, root=0)
     my_batches = comm.scatter(batches_per_rank, root=0)
 
-    # Criar uma barra de progresso compartilhada
+    # Create shared progress bar
     with Manager() as manager:
-        progress = manager.Value('i', 0)  # Contador compartilhado
-        total_batches = len(my_batches)  # NÃƒÂºmero total de lotes
+        progress = manager.Value('i', 0)  # Shared counter
+        lock = manager.Lock()
+        total_batches = len(my_batches)  # Total number of lots
+
+        local_results = []
 
         with tqdm(total=total_batches, desc=f"Processo {rank}") as pbar:
-            def update_progress(_):
-                pbar.n = progress.value  # Atualiza a barra de progresso com o valor compartilhado
-                pbar.refresh()
+            def update_progress(lock):
+                with lock:
+                    progress.value += 1
+                    pbar.update(1)
+        
+            for batch in my_batches:
+                result = process_batch(batch, output_file, tif_dates_ord)
+                local_results.extend(result)
+                update_progress(lock)
 
-            # Processar os lotes atribuÃƒÂ­dos usando ProcessPoolExecutor
-            with ProcessPoolExecutor(max_workers=cpus_slurm) as executor:
-                futures = [
-                    executor.submit(
-                        process_batch,
-                        batch,
-                        output_file,  # Caminho do sel_values
-                        tif_dates_ord,
-                        progress
-                    )
-                    for batch in my_batches
-                ]
-                for future in futures:
-                    future.add_done_callback(update_progress)
+    # Saving results locally per process
+    if local_results:
+        result_df = pd.concat(local_results, ignore_index=True)
+        result_df = explode_columns(result_df)
+        result_df.to_parquet(FOLDER_PARQUET / f'{filename}_rank_{rank}.parquet', index=False)
 
-        local_results = [future.result() for future in futures]
-
-    # Salvar os resultados localmente por processo
-    dfs = [df for results in local_results for df in results]
-    if dfs:
-        # Cria um Parquet para cada processo
-        rank_parquet_filename = FOLDER_PARQUET / f'{filename}_rank_{rank}.parquet'
-        result_df = pd.concat(dfs, ignore_index=True)
-        for col in result_df.columns:
-            if result_df[col].apply(lambda x: isinstance(x, list)).any():
-                result_df = result_df.explode(col)
-        result_df.to_parquet(rank_parquet_filename, index=False)
-
-    comm.Barrier()  # Sincronizar todos os ranks antes de continuar
+    # Sync all ranks before continuing
+    comm.Barrier()
     
     if rank == 0:
-        print(f"Todos os lotes foram processados por {size} ranks.")
+        print(f"All batches were processed by {size} ranks.")
 
-# Função para processar um único lote
-def process_batch(batch, sel_values_path, tif_dates_ord, progress):
+def process_batch(batch, sel_values_path, tif_dates_ord):
+    """
+    Processes a batch of data from an HDF5 file, extracting the required slices 
+    and passing them for further analysis.
+
+    Parameters:
+        batch (tuple): A tuple containing the start and end indices of the batch.
+        sel_values_path (str): Path to the HDF5 file containing the data.
+        tif_dates_ord (list): List of ordered dates associated with the dataset.
+
+    Returns:
+        list: A list of processed results from `runDetectionForPoint`.
+    """
     start, end = batch
     
     h5_file = h5py.File(sel_values_path, 'r')
-    # Carregar apenas o bloco específico para o lote
     sel_values_block = h5_file['values'][:, :, start:end]
     xs_slice = h5_file['xs'][start:end]
     ys_slice = h5_file['ys'][start:end]
 
-    # Criar a lista de argumentos para o processamento
     arg_list = [
         (i, sel_values_block, tif_dates_ord, xs_slice, ys_slice, NODATA_VALUE, MAX_VALUE_NDVI, FOLDER_OUTPUTS, CRS_THEIA, CRS_WGS84, img_collection)
         for i in range(sel_values_block.shape[2])
     ]
 
-    # Atualizar a barra de progresso compartilhada
-    progress.value += 1
-    
-    # Processar o lote específico
     return [runDetectionForPoint(arg) for arg in arg_list]
 
 if __name__ == '__main__':
-    # Chamar a função principal com o tamanho do lote
     main(BATCH_SIZE)
