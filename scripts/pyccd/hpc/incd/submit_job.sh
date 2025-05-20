3ae44fc9490ddad8674dffa3646b69a30943f09a
saracaetano97 <156934905+saracaetano97@users.noreply.github.com>
2025-05-20 20:22:19 +0100

    Update submit_job.sh

--- scripts/pyccd/hpc/incd/submit_job.sh
+++ scripts/pyccd/hpc/incd/submit_job.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
-#SBATCH --job-name=PyMPI              # Job Name
-#SBATCH --time=24:00:00               # Execution Time (HH:MM:SS)
-#SBATCH --nodes=5                     # Number of Nodes
-#SBATCH --ntasks-per-node=96          # Number of Tasks per Node
-#SBATCH --cpus-per-task=1            # CPUs per Task
-#SBATCH --mem-per-cpu=5333            # Memory per CPU
-#SBATCH --partition=fct               # Partition
-#SBATCH --account=cpca070342024       # Account
+#SBATCH --job-name=PyMPI              # Nome do Job
+#SBATCH --time=4-00:00:00               # Tempo de execução (HH:MM:SS)
+#SBATCH --nodes=5                     # Número de nós
+#SBATCH --ntasks-per-node=96         # Número de tarefas por nó
+#SBATCH --cpus-per-task=1            # CPUs por tarefa
+#SBATCH --mem-per-cpu=5333            # Memória por CPU
+#SBATCH --partition=fct               # Partição
+#SBATCH --account=cpca070342024       # Conta
 #SBATCH --qos=cpca070342024           # QoS
 
-# Load necessary modules
+# Carregar o Conda 
+eval "$($(which conda) shell.bash hook)"
+conda activate ccdISA
+
+# Carregar módulos necessários
 module load gcc13/openmpi/4.1.6
 
-echo "Allocated Nodes: $SLURM_NODELIST"
-echo "Allocated Tasks: $SLURM_NTASKS"
-echo "CPUs per Task: $SLURM_CPUS_PER_TASK"
-echo "CPUs in Use: $(taskset -pc $$)"
+echo "Nós alocados: $SLURM_NODELIST"
+echo "Tarefas alocadas: $SLURM_NTASKS"
+echo "CPUs por tarefa: $SLURM_CPUS_PER_TASK"
+echo "CPUs utilizados: $(taskset -pc $$)"
 
 export OMPI_MCA_btl=self,tcp
 export OMPI_MCA_orte_base_help_aggregate=0
 
-# Run the Python script with MPI
-mpiexec -n $SLURM_NTASKS python /users1/cpca070342024/scaetano/S2CHANGE/scripts/pyccd/hpc/main_mpi.py
+mpiexec -n $SLURM_NTASKS python /users1/cpca070342024/scaetano/S2CHANGE/scripts/pyccd/hpc/main_mpi2.py
